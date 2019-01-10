655cf0648452337c3ec6d95f4003e6414ea41652
Aigggerim <arystankyzy.aigerim@mail.ru>
2019-01-10 11:36:15 +0600

    Labs

---
+++ Week1/T1/T1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+
+        static bool Prime(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int cnt = 0;
+            string s = Console.ReadLine();
+            string[] s1 = Console.ReadLine().Split();
+
+            int n = int.Parse(s);
+
+            for (int i = 0; i < n; i++)
+            {
+                if (Prime(int.Parse(s1[i]))) cnt++;
+            }
+            Console.WriteLine(cnt);
+            for (int i = 0; i < n; i++)
+            {
+                if (Prime(int.Parse(s1[i]))) Console.Write(s1[i] + " ");
+            }
+        }
+    }
+}
---
+++ Week1/T3/T3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] a = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a[i] + ' ' + a[i] + ' ');
+            }
+        }
+    }
+}
