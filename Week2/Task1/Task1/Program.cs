5403ecf9b4736d5c7f9815e471778880c6c774fb
Aigggerim <arystankyzy.aigerim@mail.ru>
2019-02-15 13:31:48 +0600

    l

---
+++ Week2/Task1/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        public static bool Pal(string s)
+        {
+            for (int i = 1, j = s.Length - 1; i < j; i++, j--)
+            {
+
+                if (s[i] != s[j])
+                    return false;
+            }
+            return true;
+        }
+            static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"C:\Users\Acer\Desktop\lab2_1.txt");
+            if (Pal(lab2_1.txt))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+                Console.ReadKey();
+            }
+    }
+}
